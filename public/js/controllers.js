3164333ced7991dbca1fa5af63da2d029a1ea1c9
Krystian Jankowski <krystian.jankowski2@mail.dcu.ie>
2014-03-25 20:40:43 +0000

    Started stock page wit angular and bootstrap

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -6,6 +6,3 @@ angular.module('myApp.controllers', []).
   controller('MyCtrl1', [function() {
 
   }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
--- public/js/services.js
+++ public/js/services.js
@@ -1,9 +1,23 @@
-'use strict';
+angular.module('myApp.services', [])
 
-/* Services */
-
-
-// Demonstrate how to register services
-// In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+    .factory('stockService', [ '$http', 'globalSettings', function($http, globalSettings) {
+        return {
+            get: function(success, error) {
+                $http({
+                    method: 'GET',
+                    url: globalSettings.api_base_url + '/v1/me',
+                    headers: {
+                        'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json'
+                    }
+                }).success(function(user){
+                    console.log('sucess', user);
+                    success(user);
+                }).error(function(data) {
+                    error(data)
+                    console.log(data);
+                })
+            }
+        }
+    }]);
