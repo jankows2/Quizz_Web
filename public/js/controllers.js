6913244c90191e4105ee84b8d961a138019c3a8b
Krystian Jankowski <krystian.jankowski2@mail.dcu.ie>
2014-03-28 10:59:15 +0000

    Prod

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -9,17 +9,24 @@ angular.module('myApp.controllers', [])
 
         $scope.newsList = [];
 
-        $scope.getCompany = function (company) {
+        $scope.stockList = [];
 
-            console.log(company);
+        $scope.getCompany = function (company) {
             stockService.get(company, function (data) {
                 console.log(data);
+                angular.forEach(data, function (stocks) {
+                    $scope.stockList = [];
+                    angular.forEach(stocks, function(value) {
+                        $scope.stockList.push(value.stock)
+                        console.log(value.stock)
+                    })
+                });
             })
+            $scope.apply
 
         };
 
         $scope.getNews = function (news) {
-            console.log(news);
             stockService.getNews(news, function (data) {
                 angular.forEach(data, function (feeds) {
                     $scope.newsList = [];
