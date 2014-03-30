b939773c80920d395cee41f8a79d56d1201249de
Krystian Jankowski <krystian.jankowski2@mail.dcu.ie>
2014-03-30 18:51:21 +0100

    K-NN alogrithm start

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -19,19 +19,26 @@ angular.module('myApp.controllers', [])
 
         $scope.simpleClass = 0;
 
+        $scope.gauge_data = [];
+
+        $scope.options = {thickness: 5, mode: "gauge", total: 100};
+
+        $scope.result = [];
+
+
+
         $scope.getCompany = function (company) {
             var total=0;
             var count = 1;
             var up = 1;
             var down = 1;
             stockService.get(company, function (data) {
-                console.log(data);
                 angular.forEach(data, function (stocks) {
                     $scope.totalPrice = [];
                     $scope.stockList = [];
+                    $scope.gauge_data = [];
                     angular.forEach(stocks, function(stocks) {
                         $scope.stockList.push(stocks.stock)
-                        console.log(stocks.stock)
                         total += stocks.stock.price;
                         count = count + 1;
                         if(stocks.stock.percent_change >=0){
@@ -46,13 +53,15 @@ angular.module('myApp.controllers', [])
                 $scope.timesDown = down
                 $scope.simpleClass = ((up/30)*100).toFixed(2)
                 $scope.totalPrice = ((total/count).toFixed(2))
+                $scope.gauge_data.push(
+                    {label: "Price", value:(total/count).toFixed(2), color: "#5398f1", suffix: "$"}
+                )
             })
             stockService.getNews(company, function (data) {
                 angular.forEach(data, function (feeds) {
                     $scope.newsList = [];
                     angular.forEach(feeds, function(value) {
                         $scope.newsList.push(value.feed)
-                        console.log(value.feed)
                     })
                 });
             })
@@ -66,10 +75,22 @@ angular.module('myApp.controllers', [])
                     $scope.newsList = [];
                     angular.forEach(feeds, function(value) {
                         $scope.newsList.push(value.feed)
-                        console.log(value.feed)
                     })
                 });
             })
             $scope.apply
         };
+
+        $scope.getUnseen = function(){
+            var price = $scope.unseen.price
+            var volume = $scope.unseen.volume
+            var ytd = $scope.unseen.ytd
+            var min = 0;
+            var upDown = true;
+
+            angular.forEach($scope.stockList, function(data){
+                console.log(data.price)
+            })
+
+        }
   }])
